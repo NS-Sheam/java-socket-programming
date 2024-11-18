d3e54d660ff61be465f3d5c69235e95498b05ab9
NS-Sheam <123sheamfeni@gmail.com>
2024-11-18 23:03:39 +0600

    fix: issue in MultiThread app

--- MultiThread/src/multithread/Client.java
+++ MultiThread/src/multithread/Client.java
@@ -7,12 +7,13 @@ import java.util.*;
 public class Client {
 
     public static void main(String[] args) throws IOException {
-        String hostname = "localhost"; // Replace with server IP if running on different machines
+        String hostname = "localhost"; 
         int port = 12345;
         Socket socket = new Socket(hostname, port);
-
-        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+        System.out.println("Server Connected...");
         ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+        
         System.out.println("Enter a message: ");
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
--- MultiThread/src/multithread/MultiThreadedServer.java
+++ MultiThread/src/multithread/MultiThreadedServer.java
@@ -5,10 +5,31 @@ import java.net.*;
 
 public class MultiThreadedServer {
 
-    public static void main(String[] args) throws IOException{
+    private static int maxClient = 2;
+    private static int clientCount = 0;
+
+
+    public static void main(String[] args) throws IOException {
         int port = 12345;
-        ServerSocket serverSocket = new ServerSocket(port);
-        System.out.println("Server Started....");
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("Server Started....");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                if (clientCount < maxClient) {
+                    System.out.println("Client count: " + clientCount + 1);
+                     clientCount++;
+                    new ClientHandler(socket).start();
+                } else {
+                    socket.close();
+                    System.out.println("Max limit reached. Server closing...");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
 
@@ -22,32 +43,19 @@ class ClientHandler extends Thread {
 
     @Override
     public void run() {
+
         try {
-            ObjectOutputStream objectOutput = new ObjectOutputStream(socket.getOutputStream());
-            ObjectInputStream objectInput = new ObjectInputStream(socket.getInputStream());
-
-            Object receivedObject;
-            while ((receivedObject = objectInput.readObject()) != null) {
-                System.out.println("Received from client: " + receivedObject);
-
-                // Echo back the received object
-                objectOutput.writeObject("Server: " + receivedObject);
-                objectOutput.flush(); // Ensure the object is sent immediately
-
-                if ("bye".equalsIgnoreCase(receivedObject.toString())) {
-                    System.out.println("Client disconnected");
-                    break;
-                }
-            }
-        } catch (IOException | ClassNotFoundException ex) {
-            System.out.println("Server exception: " + ex.getMessage());
-            ex.printStackTrace();
-        } finally {
-            try {
-                socket.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
+            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            Object clientResponse = ois.readObject();
+            String clientMessage = (String) clientResponse;
+            System.out.println("From client.. " + clientMessage);
+            String serverMsg = clientMessage.toUpperCase();
+            oos.writeObject(serverMsg);
+            oos.flush();
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
